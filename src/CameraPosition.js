1cc3e298d7500b7442160c644eda84990e0943a0
antonio-munoz55 <altair555mu@gmail.com>
2025-03-14 02:45:43 +0100

    Initial commit

---
+++ src/CameraPosition.js
@@ -0,0 +1,11 @@
+export const CAMERA_POSITIONS = [
+  { x: 10, y: 45, z: 50, rotationX: 0, rotationY: 0, rotationZ: 0 },
+  { x: -10, y: 30, z: 40, rotationX: 0.1, rotationY: 0.5, rotationZ: 0 },
+  { x: 20, y: 50, z: 30, rotationX: 0.2, rotationY: -0.3, rotationZ: 0 },
+  { x: 10, y: 70, z: 10, rotationX: -1, rotationY: 0, rotationZ: 0 },
+  { x: 35, y: 45, z: 20, rotationX: 0, rotationY: 1.5, rotationZ: 0 },
+  { x: -35, y: 45, z: 20, rotationX: 0, rotationY: -1.5, rotationZ: 0 },
+  { x: 10, y: 50, z: -40, rotationX: 0, rotationY: 3.14, rotationZ: 0 },
+  { x: 0, y: 10, z: -60, rotationX: 2.5, rotationY: 0, rotationZ: 0 },
+  { x: 35, y: 110, z: 0, rotationX: -1.5, rotationY: 0, rotationZ: 0 },
+];
